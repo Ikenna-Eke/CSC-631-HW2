e5de99af89b8e9945a5dcaa48a64247d59e76af2
Ikenna N Eke <ieke@mail.sfsu.edu>
2021-02-17 23:07:51 -0800

    Added Code for Movement of player. Added Special effect Script

---
+++ Assets/SpecialEffect.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecialEffect : MonoBehaviour
+{
+    [SerializeField] private Material myColor;
+
+
+    // Update is called once per frame
+   
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+            Color random = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+            myColor.color = random;
+    }
+}
